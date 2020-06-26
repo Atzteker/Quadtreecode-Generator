fb1bc22f5cd7074e875a5104f5c3dbd8c70ff869
Atzteker <azteker007@googlemail.com>
2020-06-26 14:51:13 +0200

    add GUI class

---
+++ src/Gui.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Gui {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Quadtreecode-Generator ");
+        frame.setSize(800, 1000);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+    }
+}
