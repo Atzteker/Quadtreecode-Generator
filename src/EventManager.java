f9399f137ee61ac655ab351f6b6f827c67891867
Atzteker <azteker007@googlemail.com>
2020-06-28 22:38:26 +0200

    prepared Zahlencode generation

--- src/EventManager.java
+++ src/EventManager.java
@@ -5,6 +5,9 @@ import java.awt.event.ActionListener;
 
 public class EventManager implements ActionListener {
     private JButton[][] chessButtons = new JButton[8][8];
+
+    private enum ChessFieldState {ACTIVE, INACTIVE}
+
     private JButton[] zahlencode1LayerButtons = new JButton[4];
     private JButton[] zahlencode2LayerButtons = new JButton[16];
     private JButton[] zahlencode3LayerButtons = new JButton[64];
@@ -16,16 +19,36 @@ public class EventManager implements ActionListener {
         this.zahlencode3LayerButtons = zahlencode3LayerButtons;
     }
 
+    private void updateZahlencode(Point changedPosition, ChessFieldState chessFieldState) {
+        // layer 1 depends on layer 2 depends on layer 3
+    }
+
+    private Point getChessButtonPosition(JButton chessButton) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (chessButton == chessButtons[i][j]) {
+                    return new Point(j, i);
+                }
+            }
+        }
+        return null;
+    }
+
+    private ChessFieldState getChessFieldState(JButton chessButton) {
+        if (chessButton.getBackground() == Color.BLACK) {
+            return ChessFieldState.ACTIVE;
+        }
+        return ChessFieldState.INACTIVE;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        /*if (e.getSource() == chessButtons[0][0]){
-            chessButtons[0][0].setBackground(Color.BLACK);
-        }*/
-
         if (((JButton) e.getSource()).getBackground() == Color.WHITE) {
             ((JButton) e.getSource()).setBackground(Color.BLACK);
         } else {
             ((JButton) e.getSource()).setBackground(Color.WHITE);
         }
+
+        updateZahlencode(getChessButtonPosition((JButton) e.getSource()), getChessFieldState((JButton) e.getSource()));
     }
 }
