ad4ee0899c9c2315c7716b2d42362343f35b97a1
Atzteker <azteker007@googlemail.com>
2020-08-11 11:52:42 +0200

    updated documentation for Direction

--- src/Direction.java
+++ src/Direction.java
@@ -2,5 +2,20 @@
  * The four possible (compass)directions, where the four children of a Quad-Tree element can lie.
  */
 public enum Direction {
-    NW, NE, SE, SW
+    /**
+     * North West
+     */
+    NW,
+    /**
+     * North East
+     */
+    NE,
+    /**
+     * South West
+     */
+    SE,
+    /**
+     * South East
+     */
+    SW
 }
